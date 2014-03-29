ccaa135ef06c0ec33c57b8620b8f5d089bc2b38c
Marcus Ihde <marcusplate@googlemail.com>
2014-03-29 17:43:16 +0100

    Add tower object

--- main.lua
+++ main.lua
@@ -3,23 +3,27 @@ require "light"
 require "world"
 require "game"
 require "map"
+require "tower"
 
 function love.load()
 	G = love.graphics
-	W = love.windows
+	W = love.window
 	T = love.turris
 
 	turGame = love.turris.newGame()
 	turMap = love.turris.newMap(20, 20)
-	print(turMap.getWidth())
+	turGame.newGround("gfx/ground01.png")
+	turGame.newTower("gfx/tower01.png")
 end
 
 function love.update(dt)
 	turMap.setState(4, 3, 1)
+	turMap.setState(9, 13, 1)
 	turGame.setMap(turMap.getMap())
 end
 
 function love.draw()
+	W.setTitle("FPS: " .. love.timer.getFPS())
 	turGame.drawMap()
 end
 
