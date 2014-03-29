9f2f36c21b6ec0190b6cccd50d7face06be03f7b
Nicolai Czempin <contact@czempin.de>
2014-03-29 20:33:50 +0100

    creeps use sprites

--- game.lua
+++ game.lua
@@ -7,17 +7,21 @@ function love.turris.newGame()
   o.tower = {}
   o.enemies = {}
   o.enemyCount = 7
+  local creepImg = G.newImage("gfx/creep00_diffuse.png")
   for i=1, o.enemyCount do
-    o.enemies[i]= love.turris.newEnemy()
+    o.enemies[i]= love.turris.newEnemy("")
     o.enemies[i].x = i*60
     o.enemies[i].y = 60
+    o.enemies[i].img =  creepImg
+
   end
   o.init = function()
     o.newGround("gfx/ground01.png")
     o.newTower("gfx/tower00_diffuse.png")
-    turMap.setState(4, 3, 1)
-    turMap.setState(7, 13, 1)
+
     o.setMap(turMap.getMap())
+    o.map.setState(4, 3, 1)
+    o.map.setState(7, 13, 1)
   end
   o.update = function(dt)
     for i = 1, o.enemyCount do
@@ -51,10 +55,25 @@ function love.turris.newGame()
   o.draw = function()
     o.drawMap()
     o.drawEnemies()
+    o.drawPaths()
+  end
+  o.drawPaths = function()
+    --    for i = 1, o.entryCount do
+    --      local entry = enemyEntrances[i]
+    --    end
+    local mx, my = love.mouse.getPosition()  -- current position of the mouse
+    G.line(0,300, mx, my)
   end
   o.drawEnemies = function()
+
     for i = 1, o.enemyCount do
-      G.circle("fill", o.enemies[i].x, o.enemies[i].y, 16, 16 )
+      local e = o.enemies[i]
+      local x = e.x
+      local y = e.y
+      local img = e.img
+      G.setColor(255, 255, 255)
+      G.draw(img, x, y, 0, 1.0 / img:getWidth() * 80, 1.0 / img:getHeight() * 64)
+      -- G.circle("fill", o.enemies[i].x, o.enemies[i].y, 16, 16 )
     end
   end
   o.newGround = function(img)
