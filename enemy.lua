26c383da5a2a06275893561c6c109b982d4729ca
Nicolai Czempin <contact@czempin.de>
2014-03-30 13:25:48 +0200

    detached enemy update

--- enemy.lua
+++ enemy.lua
@@ -7,3 +7,20 @@ function love.turris.newEnemy(img)
 	o.yVel = 0.0
 	return o
 end
+function love.turris.updateEnemies(o,dt)
+		for i = 1, o.enemyCount do
+			o.enemies[i].x = o.enemies[i].x+o.enemies[i].xVel*dt
+			o.enemies[i].y = o.enemies[i].y+o.enemies[i].yVel*dt
+
+			local x = o.enemies[i].x
+			local y = o.enemies[i].y
+			if math.abs(o.baseX - x) <1 and math.abs(y - o.baseY) < 1 then
+				-- Game Over!!! (for now)
+				-- TODO: destroy ship (explosion)
+				-- TODO: destroy base (explosion!)
+				-- TODO: after explosions have finished -> transition to game over state
+				love.changegamestate(4)
+				gameOverEffect = 0
+			end
+		end
+end
--- game.lua
+++ game.lua
@@ -48,21 +48,22 @@ function love.turris.newGame()
 	end
 	o.update = function(dt)
 		o.dayTime = o.dayTime + dt * 0.1
-		for i = 1, o.enemyCount do
-			o.enemies[i].x = o.enemies[i].x+o.enemies[i].xVel*dt
-			o.enemies[i].y = o.enemies[i].y+o.enemies[i].yVel*dt
-
-			local x = o.enemies[i].x
-			local y = o.enemies[i].y
-			if math.abs(o.baseX - x) <1 and math.abs(y - o.baseY) < 1 then
-				-- Game Over!!! (for now)
-				-- TODO: destroy ship (explosion)
-				-- TODO: destroy base (explosion!)
-				-- TODO: after explosions have finished -> transition to game over state
-				love.changegamestate(4)
-				gameOverEffect = 0
-			end
-		end
+		T.updateEnemies(o,dt)
+--		for i = 1, o.enemyCount do
+--			o.enemies[i].x = o.enemies[i].x+o.enemies[i].xVel*dt
+--			o.enemies[i].y = o.enemies[i].y+o.enemies[i].yVel*dt
+--
+--			local x = o.enemies[i].x
+--			local y = o.enemies[i].y
+--			if math.abs(o.baseX - x) <1 and math.abs(y - o.baseY) < 1 then
+--				-- Game Over!!! (for now)
+--				-- TODO: destroy ship (explosion)
+--				-- TODO: destroy base (explosion!)
+--				-- TODO: after explosions have finished -> transition to game over state
+--				love.changegamestate(4)
+--				gameOverEffect = 0
+--			end
+--		end
 
 		if love.keyboard.isDown("left") then
 			o.offsetX = o.offsetX + dt * 200.0
