6c921c801c9e1fe68093181e3b371bb1c3e58dd3
Nicolai Czempin <contact@czempin.de>
2014-03-30 15:25:12 +0200

    change direction at first way point

--- enemy.lua
+++ enemy.lua
@@ -8,7 +8,7 @@ function love.turris.newEnemy(img, map)
 	o.xVel = 2.0
 	o.yVel = 0.0
 	o.waypoints = {{0,map.baseY},{1,map.baseY},{1,map.baseY-1},{3,map.baseY-1},{3,map.baseY},{map.baseX,map.baseY}}
-	o.currentWaypoint = 1
+	o.currentWaypoint = 2
 	o.health = 100.0
 	o.maxHealth = 100.0
 
@@ -20,14 +20,26 @@ end
 
 function love.turris.updateEnemies(o,dt)
 	for i = 1, o.enemyCount do
-		o.enemies[i].x = o.enemies[i].x+o.enemies[i].xVel*dt
-		o.enemies[i].y = o.enemies[i].y+o.enemies[i].yVel*dt
+		local e = o.enemies[i]
+		e.x = e.x+e.xVel*dt
+		e.y = e.y+e.yVel*dt
 
-		local x = o.enemies[i].x
-		local y = o.enemies[i].y
-		
+		local x = e.x
+		local y = e.y
+
 		-- check if waypoint reached
-		
+
+		local wp = e.waypoints[e.currentWaypoint]
+		if math.abs(wp[1]-x)<0.1 and math.abs(wp[2] -y)<0.1 then
+			-- waypoint reached
+			print ("hello")
+			e.currentWaypoint = e.currentWaypoint + 1
+			e.xVel = 0
+			e.yVel = -2
+		end
+
+		-- write back the changes
+		o.enemies[i]= e
 		-- check for and handle game over
 		if math.abs(o.baseX - x) <1 and math.abs(y - o.baseY) < 1 then
 			-- Game Over!!! (for now)
