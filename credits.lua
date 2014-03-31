d111de09cf2c44c80f400174f68609765ffe56d2
Marcus Ihde <marcusplate@googlemail.com>
2014-04-01 00:05:19 +0200

    change the credit screen a little for our neuroleptic friends :)

--- credits.lua
+++ credits.lua
@@ -1,29 +1,27 @@
-local names = {"Aldo Brießmann", "Elena Reinertz", "Marcus Ihde","Meral Leyla", "Michael Steidl", "Nicolai Czempin","Robin Kocaurek"}
+local names = { "Aldo Brießmann", "Elena Reinertz", "Marcus Ihde", "Meral Leyla", "Michael Steidl", "Nicolai Czempin", "Robin Kocaurek" }
 
 local otherText = {
-  "Latin help: Pavel Czempin",
+	"Latin help: Pavel Czempin",
 	"TESound lib: Ensayia & Taehl",
 	"Animation lib: Bart Bes",
 	"A* Search lib: RapidFire Studio",
 }
 
-
 function love.turris.drawCredits()
-	for i= 1, #names do
-		local r = math.random(0,255)
-		local g = math.random(0,255)
-		local b = math.random(0,255)
+	for i = 1, #names do
+		local r = 127 + math.sin(love.timer.getTime() * 5 - i + 90) * 127
+		local g = 127 + math.sin(love.timer.getTime() * 5 - i + 180) * 127
+		local b = 127 + math.sin(love.timer.getTime() * 5 - i + 270) * 127
 		local x = 256
-		local y = 72+i*32
+		local y = 72 + i * 32
 		love.graphics.setColor(r, g, b, 255)
 		love.graphics.print(names[i], x, y)
 	end
 
-	for i=1, #otherText do
+	for i = 1, #otherText do
 		local x = 32
-		local y = 576 - i*32
+		local y = 576 - i * 32
 		love.graphics.setColor(75, 75, 75, 255)
 		love.graphics.print(otherText[i], x, y)
-		
-end
+	end
 end
