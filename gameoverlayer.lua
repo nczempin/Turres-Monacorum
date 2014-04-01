dfac86c9f87f654fe1166e7ff5755889ffc13445
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-04-01 12:02:51 +0200

    Game over texts centered horizontally.
    Fixes issue #28.

--- gameoverlayer.lua
+++ gameoverlayer.lua
@@ -3,9 +3,9 @@ function love.turris.newGameOverLayer()
 
 	o.draw = function()
 		G.setColor(255, 0, 0, 127)
-		G.printf("Game Over!", 0, 240, 800, "center")
+		G.printf("Game Over!", 0, 240, W.getWidth(), "center")
 		G.setColor(255, 255, 255, 127)
-		G.printf("Click to go to the main menu", 0, 320, 800, "center")
+		G.printf("Click to go to the main menu", 0, 320, W.getWidth(), "center")
 		love.setgamestate(4)
 	end
 
