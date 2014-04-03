57cbcb2b38f9dd47a9943222a36aef924f019988
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-04-04 00:32:55 +0200

    Moved everything that will be used to male a release.love to its own directory

---
+++ release/main.lua
@@ -0,0 +1,155 @@
+require "libraries/gui"
+
+require "external/postshader"
+require "external/light"
+
+require "external/anim"
+require "external/TEsound"
+
+require "world"
+require "game"
+require "map"
+require "towerType"
+require "sound"
+
+require "util"
+
+function love.load()
+	G = love.graphics
+	W = love.window
+	T = love.turris
+	S = love.sounds
+
+	FONT = G.newFont(32)
+
+	currentgamestate = 0  -- 0=Main Menu 1=gameonly 4= game+gameover message
+	love.turris.selectedtower = 1 -- 1=default tower (type 1), 2=tower type 2, ...
+	love.turris.reinit()
+
+	stateMainMenu = require("state/main_menu")
+	stateCredits = require("state/credits")
+	stateSettings = require("state/settings")
+	stateSettingsVideo = require("state/settings_video")
+	stateSettingsVideoShaders = require("state/settings_video_shaders")
+	stateSettingsVideoDisplay = require("state/settings_video_display")
+
+	stateMainMenu.setVersion("v0.5.2")
+end
+
+function love.getgamestate()
+	return currentgamestate
+end
+
+function love.turris.reinit()
+	-- create game world
+	lightWorld.clearBodys()
+	love.turris.selectedtower = 1
+	turGame = love.turris.newGame()
+	turMap = love.turris.newMap(20, 20, 64, 48)
+	turGame.init()
+end
+
+function love.setgamestate(newgamestate)
+	if newgamestate == 0 then
+		stateMainMenu.effectTimer = 0
+	elseif newgamestate == 1 then
+		turGame.layerGameOver.effectTimer = 0
+	end
+
+	if currentgamestate == 5 then
+		love.sounds.stopSound("gameover")
+	elseif newgamestate == 5 then
+		love.sounds.playBackground("sounds/music/highscore.mp3", "gameover")
+	end
+
+	currentgamestate = newgamestate
+end
+
+function love.update(dt)
+	if (currentgamestate == 0)then
+		stateMainMenu.update(dt)
+	elseif (currentgamestate == 1)then
+		turGame.update(dt)
+	elseif (currentgamestate == 4)then
+		turGame.layerGameOver.update(dt)
+	elseif (currentgamestate == 5)then
+		stateCredits.update(dt)
+	elseif (currentgamestate == 6)then
+		stateSettings.update(dt)
+	elseif (currentgamestate == 7)then
+		stateSettingsVideo.update(dt)
+	elseif (currentgamestate == 8)then
+		stateSettingsVideoShaders.update(dt)
+	elseif (currentgamestate == 9)then
+		stateSettingsVideoDisplay.update(dt)
+	end
+	TEsound.cleanup()  --Important, Clears all the channels in TEsound
+end
+
+function love.draw()
+	local scanlineStrength = 4.0
+
+	G.setFont(FONT) --TODO: this only needs to be done whenever the font changes, not every frame
+	W.setTitle("FPS: " .. love.timer.getFPS())
+	love.postshader.setBuffer("render")
+	G.setColor(0, 0, 0)
+	G.rectangle("fill", 0, 0, W.getWidth(), W.getHeight())
+
+	if(currentgamestate == 0) then --render main menu only
+		stateMainMenu.draw()
+	elseif(currentgamestate == 1) then --render game only
+		turGame.draw()
+		scanlineStrength = 2.0
+	elseif(currentgamestate == 4) then -- render game + "game over" message on top
+		turGame.draw()
+		turGame.layerGameOver.draw()
+	elseif currentgamestate == 5 then --credits screen
+		stateCredits.draw()
+	elseif currentgamestate == 6 then --settings screen
+		stateSettings.draw()
+	elseif currentgamestate == 7 then --settings screen
+		stateSettingsVideo.draw()
+	elseif currentgamestate == 8 then --settings screen
+		stateSettingsVideoShaders.draw()
+	elseif currentgamestate == 9 then --settings screen
+		stateSettingsVideoDisplay.draw()
+	end
+
+	if stateSettingsVideoShaders.optionScanlines then
+		love.postshader.addEffect("scanlines", scanlineStrength)
+	end
+
+	if stateSettingsVideoShaders.optionBloom then
+		love.postshader.addEffect("bloom")
+	end
+
+	love.postshader.draw()
+end
+
+function love.turris.gameoverstate()
+	love.setgamestate(0)
+	love.turris.reinit()
+	stateMainMenu.reset()
+end
+
+function love.keypressed(key, code)
+
+end
+
+function love.mousepressed(x, y, key)
+	if(key=="l") then
+		buttonDetected = 1
+		love.turris.checkleftclick(x,y)
+	end
+	if(key=="m") then
+		buttonDetected = 3
+	end
+	if(key=="r") then
+		buttonDetected = 2
+		love.turris.checkrightclick(x,y)
+	end
+end
+
+function love.mousereleased(x, y, key)
+	buttonDetected = 0
+end
---
+++ release/state/settings_video_shaders.lua
@@ -0,0 +1,104 @@
+local o = {}
+
+local startx = W.getWidth() * 0.5 - 191 * 0.5
+local starty = 80
+
+o.imgBackground		= love.graphics.newImage("gfx/menu/menu_background.png")
+o.imgMiddleground	= love.graphics.newImage("gfx/menu/menu_middleground.png")
+o.imgScreen			= love.graphics.newImage("gfx/menu/screen00.png")
+
+o.fontMenu = G.newFont(32)
+o.fontOption = G.newFont(24)
+
+o.effectTimer = 0
+o.chromaticEffect = 0
+
+o.guiMenu			= love.gui.newGui()
+o.chkBloom			= o.guiMenu.newCheckbox(startx, starty + 64 * 0, 191, 32, true, "Bloom")
+o.chkScanlines		= o.guiMenu.newCheckbox(startx, starty + 64 * 1, 191, 32, true, "Scanlines")
+o.chkShadow			= o.guiMenu.newCheckbox(startx, starty + 64 * 2, 191, 32, true, "Shadow")
+o.chkSelfShadow		= o.guiMenu.newCheckbox(startx, starty + 64 * 3, 191, 32, true, "Self Shadow")
+o.chkGlow			= o.guiMenu.newCheckbox(startx, starty + 64 * 4, 191, 32, true, "Glow")
+o.btnBack			= o.guiMenu.newButton(startx + 8, starty + 64 * 5 + 8, 176, 34, "Back")
+
+o.optionBloom = true
+o.optionScanlines = true
+o.optionShadow = true
+o.optionGlow = true
+
+o.reset = function()
+	o.guiMenu.flushMouse()
+end
+
+o.update = function(dt)
+	o.effectTimer = o.effectTimer + dt
+	o.chromaticEffect = o.chromaticEffect + dt
+
+	o.guiMenu.update(dt)
+
+	if o.chkBloom.isHit() then
+		love.sounds.playSound("sounds/button_pressed.wav")
+		o.optionBloom = o.chkBloom.isChecked()
+	end
+
+	if o.chkScanlines.isHit() then
+		love.sounds.playSound("sounds/button_pressed.wav")
+		o.optionScanlines = o.chkScanlines.isChecked()
+	end
+
+	if o.chkShadow.isHit() then
+		love.sounds.playSound("sounds/button_pressed.wav")
+		lightWorld.optionShadows = o.chkShadow.isChecked()
+	end
+
+	if o.chkSelfShadow.isHit() then
+		love.sounds.playSound("sounds/button_pressed.wav")
+		lightWorld.optionPixelShadows = o.chkSelfShadow.isChecked()
+	end
+
+	if o.chkGlow.isHit() then
+		love.sounds.playSound("sounds/button_pressed.wav")
+		lightWorld.optionGlow = o.chkGlow.isChecked()
+	end
+
+	if o.btnBack.isHit() or love.keyboard.isDown("escape") then
+		love.sounds.playSound("sounds/button_pressed.wav")
+		love.setgamestate(6)
+		o.guiMenu.flushMouse()
+	end
+end
+
+o.draw = function()
+	G.setFont(o.fontMenu)
+	G.setBlendMode("alpha")
+	G.setColor(255, 255, 255)
+	G.draw(o.imgScreen)
+	G.setColor(255, 255, 255, 223)
+	G.draw(o.imgBackground)
+	G.setColor(95 + math.sin(o.effectTimer * 0.1) * 63, 191 + math.cos(o.effectTimer) * 31, 223 + math.sin(o.effectTimer) * 31, 255)
+	G.setBlendMode("additive")
+	G.draw(o.imgMiddleground,(W.getWidth()-o.imgMiddleground:getWidth())*0.5,0)
+
+	G.setBlendMode("alpha")
+	G.setColor(0, 0, 0, 95)
+	G.printf("Shaders", 4, 24 + 4, W.getWidth(), "center")
+	G.setColor(255, 127, 0)
+	G.setBlendMode("additive")
+	G.printf("Shaders", 0, 24, W.getWidth(), "center")
+
+	G.setFont(o.fontOption)
+	o.guiMenu.draw()
+
+	if math.random(0, love.timer.getFPS() * 5) == 0 then
+		o.chromaticEffect = math.random(0, 5) * 0.1
+	end
+
+	if o.chromaticEffect < 1.0 then
+		local colorAberration1 = math.sin(love.timer.getTime() * 10.0) * (1.0 - o.chromaticEffect) * 2.0
+		local colorAberration2 = math.cos(love.timer.getTime() * 10.0) * (1.0 - o.chromaticEffect) * 2.0
+
+		love.postshader.addEffect("chromatic", colorAberration1, colorAberration2, colorAberration2, -colorAberration1, colorAberration1, -colorAberration2)
+	end
+end
+
+return o
