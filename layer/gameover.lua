2f2499cba4add94891835adcc18ddc807e386912
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-04-08 04:31:51 +0200

    not sure why we need this; we already are in this state and it messes with playing music. If it has anything to do with the effects, we should not depend on state-setting for them

--- layer/gameover.lua
+++ layer/gameover.lua
@@ -20,7 +20,7 @@ o.draw = function()
 		G.printf("Game Over!", 0, 240, W.getWidth(), "center")
 		G.setColor(255, 255, 255, 127)
 		G.printf("Click to go to the main menu", 0, 320, W.getWidth(), "center")
-		love.setgamestate(4)
+		--love.setgamestate(4)
 	end
 end
 
