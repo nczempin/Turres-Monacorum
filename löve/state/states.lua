9e8b4761231efbb86fd5c3df627c346c27e17b0a
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-04-15 16:45:12 +0200

    missed these?

--- löve/state/states.lua
+++
@@ -1,3 +0,0 @@
-
--- GameStates:0=MainMenu, 1=inGame, 2=Load, 3=Settings, 4=Game Over, 5 = Credits
-states = {"main menu", "gameplay", "load", "settings", "game over", "credits", "settings/video","settings/video/advanced"}
