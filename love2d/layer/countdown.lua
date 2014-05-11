d413f92e60d49d33de701da9b2884078394e9211
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-05-11 21:59:50 +0200

    some more sound, and fiddling with the timing

--- love2d/layer/countdown.lua
+++ love2d/layer/countdown.lua
@@ -8,7 +8,7 @@ end
 
 o.update = function(dt)
 	o.countdown = o.countdown - dt
-	if (o.countdown <= 0)then
+	if (o.countdown <= -1)then
 		love.setgamestate(1)
 	end
 end
@@ -18,12 +18,15 @@ o.draw = function()
 	G.setColor(255, 127, 0, 127)
 	local countdown = math.floor(o.countdown+0.5)
 	local text = tostring(countdown)
-	if countdown == 0 then
+	if countdown <= 0 then
 		text = "Go!"
 	elseif countdown >= 4 then
 		text = ""
 	else if countdown >= 1 then
 		if (o.oldcountdown == countdown)then
+			if countdown == 3 then
+				love.sounds.playSound("sounds/super_phaser.mp3")
+			end
 			love.sounds.playSound("sounds/phaser_2.mp3")
 			o.oldcountdown = countdown -1
 		end
