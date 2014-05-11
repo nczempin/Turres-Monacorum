ee3ff38540394378e93547d422741f0c86a11823
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-05-11 21:53:53 +0200

    make a sound while counting down

--- love2d/layer/countdown.lua
+++ love2d/layer/countdown.lua
@@ -1,8 +1,9 @@
 local o = {}
-local SECONDS = 3
+local SECONDS = 4
 
 o.init = function(dt)
 	o.countdown = SECONDS
+	o.oldcountdown = o.countdown -1
 end
 
 o.update = function(dt)
@@ -15,9 +16,18 @@ end
 o.draw = function()
 	G.setFont(FONT_LARGE)
 	G.setColor(255, 127, 0, 127)
-	local text = tostring(math.floor(o.countdown+0.5))
-	if text == "0" then
+	local countdown = math.floor(o.countdown+0.5)
+	local text = tostring(countdown)
+	if countdown == 0 then
 		text = "Go!"
+	elseif countdown >= 4 then
+		text = ""
+	else if countdown >= 1 then
+		if (o.oldcountdown == countdown)then
+			love.sounds.playSound("sounds/phaser_2.mp3")
+			o.oldcountdown = countdown -1
+		end
+	end
 	end
 	G.printf(text, 0, 240, W.getWidth(), "center")
 	G.setColor(255, 255, 255, 127)
