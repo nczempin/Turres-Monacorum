d313b5a5cdcc4e392d1d56993889f3c9e1a3d2a1
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-05-14 21:09:32 +0200

    added "ok" button to briefing

--- love2d/layer/missionBriefing.lua
+++ love2d/layer/missionBriefing.lua
@@ -2,19 +2,29 @@ local o = {}
 
 o.text = ""
 
-o.init = function(text)
+o.guiMenu = love.gui.newGui()
+o.init = function(text, mission)
 	o.text = text
+	o.btnOkay = o.guiMenu.newButton(W.getWidth()/2, W.getHeight()*0.75, 66, 42, "OK")
+	o.mission = mission
 end
 
 o.update = function(dt)
+	o.guiMenu.update(dt)
+	if o.btnOkay.isHit() then
+		love.sounds.playSound("sounds/button_pressed.wav")
+		love.setgamestate(14, o.mission)
+		o.guiMenu.flushMouse()
+	end
 end
 
 o.draw = function()
-	G.setFont(FONT_LARGE)
+	G.setFont(FONT)
 	G.setColor(255, 127, 0, 127)
 	local scale = 1---(o.countdown-o.oldcountdown)/2
 	G.printf(o.text, 0, 240, W.getWidth(), "center",0,scale,scale)
 	G.setColor(255, 255, 255, 127)
+	o.guiMenu.draw()
 end
 
 return o
