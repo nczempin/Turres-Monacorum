91d05d0f87bec4facc0f44781d7c05e81faced40
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-05-17 21:05:59 +0200

    deactivate the debug mission for regular gameplay

--- love2d/data/menu/missions1.lua
+++ love2d/data/menu/missions1.lua
@@ -1,13 +1,13 @@
 missions = {
-	{
-		map = "debug1",
-		x = 150,
-		y = 130,
-		title = "Debug 1",
-		description = "Debug 1.\n\nMapsize: Small",
-		briefingText = "For trying out new features efficiently",
-		button = "D",
-	},
+--	{
+--		map = "debug1",
+--		x = 150,
+--		y = 130,
+--		title = "Debug 1",
+--		description = "Debug 1.\n\nMapsize: Small",
+--		briefingText = "For trying out new features efficiently",
+--		button = "D",
+--	},
 	{		map = "tutorial1",
 		x = 300,
 		y = 130,
