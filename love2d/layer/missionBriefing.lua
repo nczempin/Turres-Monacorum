1028971a3f41fea7b1c55c41659cdf50115ef83f
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-05-17 17:56:11 +0200

    moved some stuff around so that messages are drawn consistently

--- love2d/layer/missionBriefing.lua
+++ love2d/layer/missionBriefing.lua
@@ -27,20 +27,11 @@ o.draw = function()
 	local text = o.text
 	local scale = 1
 	local alignment = "left"
-	o.drawMessage(x1,x2,y1,rectX1,text,scale,alignment)
+	love.turris.drawMessage(x1,x2,y1,rectX1,text,scale,alignment)
 	G.setColor(255, 255, 255, 127)
 	G.setFont(FONT)
 	o.guiMenu.draw()
 end
 
-o.drawMessage = function(x1,x2,y1,rectX1,text,scale,alignment)
-	love.postshader.addEffect("monochrom")
-	G.setFont(FONT_SMALLER)
-	G.setColor(0, 0, 0, 127)
-	G.rectangle("fill", rectX1, 160 - 16, x2 + 32, 256 + 32);
-	G.setColor(255, 127, 0)
-	G.rectangle("line", rectX1+4, 160 - 12, x2 + 24, 256 + 24);
-	G.setColor(255, 127, 0)
-	G.printf(text, x1, y1, x2, alignment, 0, scale, scale)
-end
+
 return o
--- love2d/util.lua
+++ love2d/util.lua
@@ -10,3 +10,14 @@ function distance_euclid(x1,y1,x2,y2)
 	local y = y1-y2
 	return math.sqrt(x*x+y*y)
 end
+
+love.turris.drawMessage = function(x1,x2,y1,rectX1,text,scale,alignment)
+	love.postshader.addEffect("monochrom")
+	G.setFont(FONT_SMALLER)
+	G.setColor(0, 0, 0, 127)
+	G.rectangle("fill", rectX1, 160 - 16, x2 + 32, 256 + 32);
+	G.setColor(255, 127, 0)
+	G.rectangle("line", rectX1+4, 160 - 12, x2 + 24, 256 + 24);
+	G.setColor(255, 127, 0)
+	G.printf(text, x1, y1, x2, alignment, 0, scale, scale)
+end
