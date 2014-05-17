b903c033a93165e4b2fe954e0ce62bcc73ae56da
Nicolai Czempin <nicolai.czempin@gmail.com>
2014-05-17 20:59:12 +0200

    added debug missions

--- love2d/data/menu/missions1.lua
+++ love2d/data/menu/missions1.lua
@@ -1,6 +1,14 @@
 missions = {
 	{
-		map = "tutorial1",
+		map = "debug1",
+		x = 150,
+		y = 130,
+		title = "Debug 1",
+		description = "Debug 1.\n\nMapsize: Small",
+		briefingText = "For trying out new features efficiently",
+		button = "D",
+	},
+	{		map = "tutorial1",
 		x = 300,
 		y = 130,
 		title = "Tutorial 1",
