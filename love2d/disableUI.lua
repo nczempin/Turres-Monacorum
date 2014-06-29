ef589c5e920c3295fe965b99d3bf1e42ab260183
Nicolai Czempin <contact@czempin.de>
2014-06-29 20:17:03 +0200

    add the factored out module

---
+++ love2d/disableUI.lua
@@ -0,0 +1,12 @@
+
+local o = {}
+
+o.massDisplayDisabled = true
+o.energyDisplayDisabled = true
+
+
+o.laserTowerDisplayDisabled = true
+o.energyTowerDisplayDisabled = true
+o.massTowerDisplayDisabled = true
+
+return o
