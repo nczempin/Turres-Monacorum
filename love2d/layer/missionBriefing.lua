b72432f8cde685cfa72ed6ae7497eeaafd211b13
Nicolai Czempin <contact@czempin.de>
2014-07-19 12:46:05 +0200

    skip map briefing when no briefing text

--- love2d/layer/missionBriefing.lua
+++ love2d/layer/missionBriefing.lua
@@ -1,36 +1,45 @@
 local o = {}
 
 o.text = ""
-
+o.active = false
 o.guiMenu = love.gui.newGui()
 o.init = function(mission)
-	o.text = mission.briefingText or mission.description
-	o.btnOkay = o.guiMenu.newButton(W.getWidth() * 0.5 - 16, 364, 66, 42, "OK")
-	o.mission = mission
-	turGame.map.editMode = false;
+  o.text = mission.briefingText --or mission.description
+  if o.text == null then
+    o.active = false
+    love.setgamestate(14, o.mission)
+
+  else
+    o.active = true
+    o.btnOkay = o.guiMenu.newButton(W.getWidth() * 0.5 - 16, 364, 66, 42, "OK")
+    o.mission = mission
+    turGame.map.editMode = false;
+  end
 end
 
 o.update = function(dt)
-	o.guiMenu.update(dt)
-	if o.btnOkay.isHit() then
-		love.sounds.playSound("sounds/button_pressed.wav")
-		love.setgamestate(14, o.mission)
-		o.guiMenu.flushMouse()
-	end
+  o.guiMenu.update(dt)
+  if not o.btnOkay or  o.btnOkay.isHit() then
+    love.sounds.playSound("sounds/button_pressed.wav")
+    love.setgamestate(14, o.mission)
+    o.guiMenu.flushMouse()
+  end
 end
 
 o.draw = function()
-	local x1 = W.getWidth() * 0.125
-	local x2 = W.getWidth() * 0.75
-	local y1 = 160
-	local rectX1 = x1 - 16
-	local text = o.text
-	local scale = 1
-	local alignment = "left"
-	love.turris.drawMessage(x1,x2,y1,rectX1,text,scale,alignment)
-	G.setColor(255, 255, 255, 127)
-	G.setFont(FONT)
-	o.guiMenu.draw()
+  local x1 = W.getWidth() * 0.125
+  local x2 = W.getWidth() * 0.75
+  local y1 = 160
+  local rectX1 = x1 - 16
+  local text = o.text
+  local scale = 1
+  local alignment = "left"
+  if o.active then
+    love.turris.drawMessage(x1,x2,y1,rectX1,text,scale,alignment)
+  end
+  G.setColor(255, 255, 255, 127)
+  G.setFont(FONT)
+  o.guiMenu.draw()
 end
 
 
